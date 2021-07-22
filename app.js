4af45dc4cceb538d798370275ed77ecdf4c5d6d7
Rafael Pazini <rflpazini@gmail.com>
2021-07-22 09:48:40 -0300

    add: unique names for project

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const app = express();
 
-app.use(express.json())
+const { uniqueNamesGenerator, adjectives, animals } = require('unique-names-generator');
+
+app.use(express.json());
+
+const appName = uniqueNamesGenerator({
+  dictionaries: [adjectives, animals], 
+  length: 2
+}).toUpperCase(); 
 
 app.get('/', (req, res) => {
-    res.send({ message:"Hello World from DOCKER!" })
+    res.send({ message:`Hello World from ${appName}!` })
 })
 
 const port = process.env.PORT || 3000,
   server = app.listen(port, () => {
-    console.log('🐋 ... is running at http://localhost:%d', port);
+    console.log('🐋 %s is running at http://localhost:%d', appName, port);
   });
 
 module.exports = server;
